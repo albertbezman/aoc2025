f1c750b89313ca9a965cd4d930780ab02b4bee8d
Albert Bezman <albert@bezman.dev>
2025-11-30 20:04:34 +0000

    add basic python boilerplate

---
+++ py/src/aoc2025/day1.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import sys
+
+
+def part1(input_text: str) -> int:
+    # TODO: implement solution
+    return 0
+
+
+def part2(input_text: str) -> int:
+    # TODO: implement solution
+    return 0
+
+
+def main() -> None:
+    data = sys.stdin.read().rstrip("\n")
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
+
+
+if __name__ == "__main__":
+    main()
